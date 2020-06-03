d04e06a44349f343f6d240c16952d4219d6ee2ee
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2020-06-03 13:14:36 -0400

    refactor to work with custom app

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Vanilla RichText Editor
 
-This is a simple CMS that uses CKEditor and a cloudinary image upload adapter
+This is a simple CMS that uses CKEditor and a custom image upload adapter
 
 ## How it works
 
 - I used instructions found [here](https://ckeditor.com/docs/ckeditor5/latest/builds/guides/quick-start.html) to create the editor.
-- I added a 'EditorSetup' script which contains the class for the CKEditor-Cloudinary-Image-Upload-Adapter.
+- I added a 'EditorSetup' script which contains the class for the Custom-Image-Upload-Adapter.
 - I then used instructions found [here](https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/configuration.html#adding-features) to setup the adaptor with CKEditor.
-- You will need an unsigned upload preset for this to work properly. Find instructions for setting that up [here](https://support.cloudinary.com/hc/en-us/articles/360004967272-Upload-Preset-Configuration)
+- You can find a sample backend app to use with this [here](https://github.com/IyiKuyoro/Sample-Image-Upload-App)
--- js/EditorSetup.js
+++ js/EditorSetup.js
@@ -1,12 +1,8 @@
-class CloudinaryImageUploadAdapter {
-  constructor(loader, cloudName, unsignedUploadPreset, sizes) {
+class CustomImageUploadAdapter {
+  constructor(url, loader) {
     this.loader = loader;
     this.xhr = new XMLHttpRequest();
-    this.cloudName = cloudName;
-    this.unsignedUploadPreset = unsignedUploadPreset;
-    if (sizes) {
-      this.sizes = sizes;
-    }
+    this.url = url;
   }
 
   upload() {
@@ -14,9 +10,8 @@ class CloudinaryImageUploadAdapter {
       (file) =>
         new Promise((resolve, reject) => {
           const fd = new FormData();
-          const url = `https://api.cloudinary.com/v1_1/${this.cloudName}/upload`;
-          this.xhr.open('POST', url, true);
-          this.xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+          this.xhr.open('POST', this.url, true);
+          // this.xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 
           // Hookup an event listener to update the upload progress bar
           this.xhr.upload.addEventListener('progress', e => {
@@ -26,34 +21,24 @@ class CloudinaryImageUploadAdapter {
 
           // Hookup a listener to listen for when the request state changes
           this.xhr.onreadystatechange = () => {
-            if (this.xhr.readyState === 4 && this.xhr.status === 200) {
+            if (this.xhr.readyState === 4 && this.xhr.status === 201) {
               // Successful upload, resolve the promise with the new image
               const response = JSON.parse(this.xhr.responseText);
 
-              let images;
-
-              if (this.sizes) {
-                images = {
-                  default: response.secure_url,
-                  ...this._getImageSizes(response.secure_url),
-                };
-              } else {
-                images = {
-                  default: response.secure_url,
-                };
-              }
+              let images = {
+                default: response.secure_url,
+              };
 
               resolve(images);
-            } else if (this.xhr.status !== 200) {
+            } else if (this.xhr.status !== 201) {
+              console.log(this.xhr.status)
               // Unsuccessful request, reject the promise
               reject('Upload failed');
             }
           };
 
           // Setup the form data to be sent in the request
-          fd.append('upload_preset', this.unsignedUploadPreset);
-          fd.append('tags', 'browser_upload');
-          fd.append('file', file);
+          fd.append('image', file);
           this.xhr.send(fd);
         }),
     );
@@ -65,50 +50,13 @@ class CloudinaryImageUploadAdapter {
       this.xhr.abort();
     }
   }
-
-  _getImageSizes(defaultImageUrl) {
-    const imageObject = {};
-
-    // Split url in two
-    const splitUrl = this._splitUrl(defaultImageUrl);
-
-    if (this.sizes) {
-      const len = this.sizes.length;
-      this.sizes.forEach((size, index) => {
-        if (index !== len - 1) {
-          imageObject[size.toString()] = `${splitUrl.firstHalf}w_${size}%2Cc_scale${splitUrl.secondHalf}`;
-        } else {
-          imageObject[size.toString()] = defaultImageUrl;
-        }
-      });
-    }
-
-    return imageObject;
-  }
-
-  _splitUrl(url) {
-    // This function splits the image url in two.
-    const firstHalfLength = 41 + this.cloudName.length;
-    const firstHalf = url.substr(0, firstHalfLength);
-    const secondHalf = url.substr(firstHalfLength - 1, url.length - firstHalfLength + 1);
-
-    return {
-      firstHalf,
-      secondHalf,
-    };
-  }
 }
 
 
 // This is function is used to create the upload adaptor for CKEditor
 function MyUploadAdapterPlugin( editor ) {
   editor.plugins.get( 'FileRepository' ).createUploadAdapter = function( loader ) {
-    return new CloudinaryImageUploadAdapter( 
-      loader,
-      'your_cloud_name',
-      'your_unsigned_upload_preset',
-      [ 160, 500, 1000, 1052 ]
-    );
+    return new CustomImageUploadAdapter('http://localhost:4000/upload', loader);
   };
 }
 
